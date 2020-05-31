b091ebbe59f99cf1df6eeeb0149fc1014627f854
Spalletta, Esteban <esteban.spalletta@accenture.com>
2020-05-31 20:45:08 -0300

    agregar filtro y sort

--- ejercicio/src/app/people/people-list/people-list.component.ts
+++ ejercicio/src/app/people/people-list/people-list.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { PeopleService } from '../services/people.service';
-import { ReplaySubject } from 'rxjs';
-import { People } from '../models/people';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-people',
@@ -9,13 +9,14 @@ import { People } from '../models/people';
   styleUrls: ['./people-list.component.css']
 })
 export class PeopleListComponent implements OnInit {
-  public dataSource$: ReplaySubject<People[]> = new ReplaySubject(1);
   public displayedColumns: string[]
+  public dataSource = new MatTableDataSource();
   
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
   
   constructor(private peopleService: PeopleService ) { 
     this.peopleService.getPeopleList().subscribe( obs => {
-      this.dataSource$.next(obs);
+      this.dataSource.data = obs
     })
   }
 
@@ -23,7 +24,13 @@ export class PeopleListComponent implements OnInit {
     this.displayedColumns = listColumns
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
   ngOnInit(): void {
+    this.dataSource.sort = this.sort;
   }
 
 }
--- ejercicio/src/app/shared/shared.module.ts
+++ ejercicio/src/app/shared/shared.module.ts
@@ -9,6 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [],
@@ -21,7 +22,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatIconModule,
     MatButtonModule,
     MatTableModule,
-    MatSelectModule
+    MatSelectModule,
+    MatSortModule
   ],
   exports: [
     MatCardModule,
@@ -32,7 +34,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatIconModule,
     MatButtonModule,
     MatTableModule,
-    MatSelectModule
+    MatSelectModule,
+    MatSortModule
   ]
 })
 
